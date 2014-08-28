cc9b8223af60fdb79989195611c95678f3d711a1
Ram Ratan Maurya <ratan.maurya@gmail.com>
2014-08-28 10:40:42 +0530

    Bump version to 1.0.9

--- stag-custom-sidebars.php
+++ stag-custom-sidebars.php
@@ -3,7 +3,7 @@
  * Plugin Name: Stag Custom Sidebars
  * Plugin URI: https://wordpress.org/plugins/stag-custom-sidebars
  * Description: Create custom dynamic sidebars and use anywhere with shortcodes.
- * Version: 1.0.8
+ * Version: 1.0.9
  * Author: Ram Ratan Maurya
  * Author URI: https://mauryaratan.me
  * Requires at least: 3.3
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  *
  * @package Stag_Custom_Sidebars
  * @author Ram Ratan Maurya
- * @version 1.0.8
+ * @version 1.0.9
  * @copyright 2014 Ram Ratan Maurya
  */
 final class Stag_Custom_Sidebars {
@@ -35,7 +35,7 @@ final class Stag_Custom_Sidebars {
 	/**
 	 * @var string
 	 */
-	public $version = '1.0.8';
+	public $version = '1.0.9';
 
 	/**
 	 * @var string
